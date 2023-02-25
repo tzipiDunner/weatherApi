4d5b695644fca5b4254cbdeb7cec6263569fa489
tzipiDunner <116270164+tzipiDunner@users.noreply.github.com>
2023-02-26 01:41:28 +0200

    Update WeatherService.cs

--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -13,7 +13,7 @@ namespace Services
                 var response = await client.GetAsync($"https://api.weatherapi.com/v1/forecast.json?key=39f8ecaf506c4f76b3f55139222906&q={city}&days=1&api=yes&alerts=yes");
                 response.EnsureSuccessStatusCode();
                 string stringResult = await response.Content.ReadAsStringAsync();
-                JObject json = JObject.Parse(stringResult);//??????????????
+                JObject json = JObject.Parse(stringResult);
                 string temp_c = json["current"]["temp_c"].ToString();
                 string condition = json["current"]["condition"]["text"].ToString();
                 return $"The weather in {city} is: temp {temp_c}, condition {condition}";
